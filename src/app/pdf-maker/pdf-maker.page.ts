8910f66613d2a144e095c7de87364a52c2b96eb5
unknown <costa.carlos12@gmail.com>
2020-04-13 17:59:21 -0300

    zip

--- src/app/pdf-maker/pdf-maker.page.ts
+++ src/app/pdf-maker/pdf-maker.page.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import pdfMake from 'pdfmake/build/pdfmake';
-import pdfFonts from 'pdfmake/build/vfs_fonts';
+import "file-saver";
+
+const pdfFonts = require("pdfmake/build/vfs_fonts");
+const pdfMake = require("pdfmake/build/pdfmake");
+const htmlToPdfmake = require("html-to-pdfmake");
+const JSZip = require("jszip");
 
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
-const htmlToPdfmake = require('html-to-pdfmake');
 
 @Component({
-  selector: 'app-pdf-maker',
-  templateUrl: './pdf-maker.page.html',
-  styleUrls: ['./pdf-maker.page.scss']
+  selector: "app-pdf-maker",
+  templateUrl: "./pdf-maker.page.html",
+  styleUrls: ["./pdf-maker.page.scss"],
 })
 /** This page uses pdfMake and htmlToPdfmake as core libraries to convert html to PDF file.
  *  At the moment (27/03/2020) we can generate and download a single html as PDF.
@@ -33,30 +36,31 @@ export class PDFMAKERPage implements OnInit {
   async createPDF() {
     this.createButtonDisable = true;
     const html = await htmlToPdfmake(
-      document.getElementById('printable').innerHTML
+      document.getElementById("printable").innerHTML
     );
-    console.log('html:', html);
+    console.log("html:", html);
     let docDefinition = {
-      header: 'teste',
+      header: "teste",
       content: [html],
       footer: (currentPage) => {
         return {
           margin: 10,
           columns: [
             {
-              text: currentPage.toString()
-            }
-          ]
+              text: currentPage.toString(),
+            },
+          ],
         };
-      }
+      },
     };
     this.pdfObject = pdfMake.createPdf(docDefinition);
-    this.pdfObject.getBase64(pdf => {
-      window.open(
-        ('data:application/pdf;base64,' + pdf) as 'teste'
-      ).document.title = `ESSE EH UM PDF`;
+    this.pdfObject.getBase64((pdf) => {
+      // window.open(
+      //   ("data:application/pdf;base64," + pdf) as "teste"
+      // ).document.title = `ESSE EH UM PDF`;
       this.zipObject.push(pdf);
-      console.log(typeof pdf);
+      console.log(`pdf:`, typeof pdf);
+      console.log(pdf);
     });
 
     console.log(this.zipObject);
@@ -68,12 +72,17 @@ export class PDFMAKERPage implements OnInit {
 
   /** Download pdfObject by pdfMake.prototype.download(~file_name~) */
   downloadPdf() {
-    this.pdfObject.download('teste.pdf');
+    this.pdfObject.download("teste.pdf");
   }
 
-  downloadPdfZip() {
-    const blob = new Blob(this.zipObject, { type: 'application/zip' });
-    const url = window.URL.createObjectURL(blob);
-    window.open(url);
+  async downloadPdfZip() {
+    let zip = new JSZip();
+    for (let index = 0; index < this.zipObject.length; index++) {
+      zip.file(`${index}.pdf`, this.zipObject[index], { base64: true });
+    }
+    // zip.file("dynamic-pleading.pdf", pdf64, { base64: true });
+    zip.generateAsync({ type: "blob" }).then((content) => {
+      saveAs(content, "teste.zip");
+    });
   }
 }
